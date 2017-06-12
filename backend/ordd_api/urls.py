f84822c1e29e43f7009bbfe9e0c799f280a99d62
Matteo Nastasi <nastasi@openquake.org>
2017-06-12 10:28:06 +0200

    add 'profile' view

--- backend/ordd_api/urls.py
+++ backend/ordd_api/urls.py
@@ -1,13 +1,14 @@
 # ordd_api/urls.py
 
-from django.conf.urls import url, include
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import RegionCreateView, CountryCreateView, UserRetrieveView
+from .views import RegionCreateView, CountryCreateView, ProfileCreateView, UserCreateView
 
 urlpatterns = {
-    url(r'^user/$', UserRetrieveView, name="user_retrieve"),
+    url(r'^profile$', ProfileCreateView, name="profile_create"),
+    url(r'^user/$', UserCreateView, name="user_create"),
     url(r'^region/$', RegionCreateView.as_view(), name="region_create"),
     url(r'^country/$', CountryCreateView.as_view(), name="country_create"),
     
--- backend/ordd_api/views.py
+++ backend/ordd_api/views.py
@@ -1,11 +1,14 @@
 # api_exp01/views.py
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth import get_user
 # from .permissions import IsOwner
+from ordd.settings import ORDD_API_BASEPATH
 from .serializers import RegionSerializer, CountrySerializer, UserSerializer
 from .models import Region, Country
 
@@ -14,16 +17,26 @@ from .models import Region, Country
 #    permission_classes = (
 #        permissions.IsAuthenticated, IsOwner)
 
-
-# class UserGetView(generics.RetrieveAPIView):
-#     serializer_class = serializers.PostSerializer
 @api_view(['GET'])
-def UserRetrieveView(request):
+def ProfileCreateView(request):
     """This view return base user fields, included groups"""
+    if not request.user.is_authenticated:
+        raise PermissionDenied()
+
     instance = get_user(request)
     serializer = UserSerializer(instance)
     return Response(serializer.data)
 
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def UserCreateView(request):
+    if not request.user.is_authenticated:
+        raise PermissionDenied()
+
+    user = get_user(request)
+    if not user.groups.filter(name='admin'):
+        raise PermissionDenied()
+
+    return Response({'answer': 'Work In Progress'})
 
 class RegionCreateView(generics.ListCreateAPIView):
     """This class handles the GET and POSt requests of our rest api."""
