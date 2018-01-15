88fecb623eb44bb9442faf861c0c0ebd1bf8e310
Manuel <manuel.cavallaro@cimafoundation.org>
2018-01-15 16:13:30 +0100

    Client Update

--- frontend/js/angular/app.js
+++ frontend/js/angular/app.js
@@ -8,7 +8,7 @@ var baseUrl = 'http://localhost:63342/RODI/frontend/';
 // var baseUrl = 'http://localhost:63342/open-risk-data-dashboard/frontend/';
 //var baseUrl = 'http://www.riskopendata.org/';
 
-var baseAPIurl = 'https://www.riskopendata.org/api/'; // Sviluppo
+var baseAPIurl = 'https://dev.riskopendata.org/api-dev/'; // Sviluppo
 var APIversion = '0.27.0';
 
 RodiApp.config(function($locationProvider) {
