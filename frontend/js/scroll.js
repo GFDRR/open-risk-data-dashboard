ee20eff1ba6eb410aed6e4ee11534abfaaef0330
pzwsk <pierre.chrzanowski@gmail.com>
2018-02-16 10:26:06 +0100

    Changes to home, doc and css mainly

--- frontend/js/scroll.js
+++ frontend/js/scroll.js
@@ -1,23 +1,25 @@
 /**
  * Created by Manuel on 21/12/2016.
- */
+ 
 
 $(function() {
-    $('#startReadingBtn').click(function() {
+    /*$('#startReadingBtn').click(function() {
         $('html, body').animate({
             scrollTop: $("#startReading").offset().top
         }, 1000);
-    });
+    });*/
 
     $('a[href*="#"]:not([href="#"])').click(function() {
         //if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
+               /* $('html, body').animate({*/
+                var scroll_position = target.offset().top;
+                alert(scroll_position);
+                    scrollTop: scroll_position;
+               /* }, 1000);
+            return false;*/
             }
         //}
     });
