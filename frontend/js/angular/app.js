542f321cbd459c3c19588f81994f25d33190e35a
pzwsk <pierre.chrzanowski@gmail.com>
2018-12-18 15:10:15 +0100

    Update app.js

--- frontend/js/angular/app.js
+++ frontend/js/angular/app.js
@@ -31,8 +31,8 @@ var baseUrl = spli_url.protocol + '//' + spli_url.host + '/';
 //     to have full access to development backend from
 //     your local development installation
 //
-var baseAPIurl = '/api/';
-//var baseAPIurl = 'https://dev.riskopendata.org/api/';
+//var baseAPIurl = '/api/';
+var baseAPIurl = 'https://dev.riskopendata.org/api/';
 
 RodiApp.config(function($locationProvider) {
 
